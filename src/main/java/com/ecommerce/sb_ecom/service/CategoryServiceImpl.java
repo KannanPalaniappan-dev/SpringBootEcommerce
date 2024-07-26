f83cd2488a282e0689d8794316cc2004cb79f4f9
kannan-palaniappan <kannansiva8080@gmail.com>
2024-07-27 00:57:15 +0530

    Managing Id's

--- src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -3,12 +3,15 @@ package com.ecommerce.sb_ecom.service;
 import com.ecommerce.sb_ecom.model.Category;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
+
+    private Long nextId = 1L;
     @Override
     public List<Category> getAllCategories() {
         return categories;
@@ -16,6 +19,7 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
 }
